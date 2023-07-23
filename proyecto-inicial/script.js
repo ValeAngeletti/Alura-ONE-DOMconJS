042b8ad40857bab65770f9af9bf18ac4ef72b67f
Vale Angeletti <valeangelett11@gmail.com>
2023-07-23 14:06:59 -0300

    Clase 02 Completada

---
+++ proyecto-inicial/script.js
@@ -0,0 +1,16 @@
+ const btn = document.querySelector("[data-form-btn]");
+
+ const createTask =  (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]")
+    console.log(input.value);
+}
+
+ console.log(btn);
+
+
+ //listener  --> 'escucha el click o evento'
+
+// Arrow functions o funciones anónimas 
+btn.addEventListener("click", createTask);
+
