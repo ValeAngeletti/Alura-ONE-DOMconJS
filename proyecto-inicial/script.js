6fc5c8bf1003b0864dc027ab66a67d36bd989e2d
Vale Angeletti <valeangelett11@gmail.com>
2023-07-24 16:11:44 -0300

    Proyecto Completado

--- proyecto-inicial/script.js
+++ proyecto-inicial/script.js
@@ -1,7 +1,7 @@
-( () => { 
+import checkComplete from "../componentes/checkComplete.js";
+import deleteIcon from "../componentes/deleteIcon.js";
 
     const btn = document.querySelector("[data-form-btn]");
-
 
     const createTask =  (evento) => {
         evento.preventDefault();
@@ -17,32 +17,9 @@
         titleTask.classList.add("task");
         titleTask.innerText = value; 
         taskContent.appendChild(titleTask);
-        const content = `      
-            <i class="fas fa-trash-alt trashIcon icon"></i>`
-
-        //task.innerHTML = content;
         task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
         list.appendChild(task);
-    }
-    //listener  --> 'escucha el click o evento'
-    // Arrow functions o funciones anónimas 
+    };
 
     btn.addEventListener("click", createTask);
-
-    const checkComplete = () => {
-
-        const i = document.createElement("i");
-        i.classList.add("far", "fa-check-square", "icon");
-        i.addEventListener("click", completeTask);
-        return i;
-    }
-
-    const completeTask = (event) => {
-        const element = event.target;
-        element.classList.toggle('fas');
-        element.classList.toggle('completeIcon');
-        element.classList.toggle('far');
-
-    }
-
-}) ();
